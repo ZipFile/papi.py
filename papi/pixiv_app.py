4c9e80977cbbe41a5b9227cb33db233da0d2f0f3
Anatoliy Anischovich <lin.aaa.lin@gmail.com>
2016-04-06 23:34:07 +0300

    Update pixiv devices

--- papi/pixiv_app.py
+++ papi/pixiv_app.py
@@ -5,19 +5,16 @@ PixivApp = namedtuple("PixivApp", [
     "user_agent", "client_id", "client_secret", "access_token"
 ])
 
-android_4_6_0 = PixivApp(
-    user_agent="PixivAndroidApp/4.6.0",
+android = PixivApp(
+    user_agent="PixivAndroidApp/4.6.9",
     client_id="BVO2E8vAAikgUBW8FYpi6amXOjQj",
     client_secret="LI1WsFUDrrquaINOdarrJclCrkTtc3eojCOswlog",
     access_token="8mMXXWT9iuwdJvsVIvQsFYDwuZpRCMePeyagSh30ZdU",
 )
 
-ios_5_1_1 = PixivApp(
-    user_agent="PixivIOSApp/5.1.1",
+ios = PixivApp(
+    user_agent="PixivIOSApp/5.8.7",
     client_id="bYGKuGVw91e0NMfPGp44euvGt59s",
     client_secret="HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK",
     access_token=None,
 )
-
-android = android_4_6_0
-ios = ios_5_1_1
